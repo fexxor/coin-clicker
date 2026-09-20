7517225c0a91222e64855774ff2dfdcd9c3888e5
Felix <>
2026-09-20 18:27:57 +0200

    Optimize employee images

--- src/employees/employees.jsx
+++ src/employees/employees.jsx
@@ -1,4 +1,17 @@
 import { prettyPrintNumber } from "../lib/prettyPrintNumber";
+import { intern, juniorEmployee, seniorEmployee, engineer, scientist, robot, AISingularity } from "./employees";
+
+const MAX_EMPLOYEES_DISPLAYED = 56;
+
+const EMPLOYEES_MAX_PLACEHOLDER = {
+  intern: Array(MAX_EMPLOYEES_DISPLAYED).fill(intern.image),
+  junior: Array(MAX_EMPLOYEES_DISPLAYED).fill(juniorEmployee.image),
+  senior: Array(MAX_EMPLOYEES_DISPLAYED).fill(seniorEmployee.image),
+  engineer: Array(MAX_EMPLOYEES_DISPLAYED).fill(engineer.image),
+  scientist: Array(MAX_EMPLOYEES_DISPLAYED).fill(scientist.image),
+  robot: Array(MAX_EMPLOYEES_DISPLAYED).fill(robot.image),
+  AI_singularity: Array(MAX_EMPLOYEES_DISPLAYED).fill(AISingularity.image),
+}
 
 export function EmployeeList({employees, type, employeeMultiplier = 1, buildingEffects = {}}) {
   const employeesOfType = employees.filter((employee) => employee.type === type);
@@ -14,17 +27,21 @@ export function EmployeeList({employees, type, employeeMultiplier = 1, buildingE
   return (
     employeesOfType.length > 0 && (
       <>
-        <h3>
+        <h3 title={"\nProduction rate per employee: " + prettyPrintNumber((employees[0].productionRate * adjustedEmployeeMultiplier).toFixed(2))}>
           {heading} ({numberOfEmployees})
         </h3>
         <p className={type}>
-          {employeesOfType.slice(0, 150).map((employee, i) => (
-            <span key={`${type}-${i}`} title={employee.name + "\nProduction Rate: " + prettyPrintNumber((employee.productionRate * adjustedEmployeeMultiplier).toFixed(2))}>
-              {employee.image}
-            </span>
-          ))}
+          {employeesOfType.length < MAX_EMPLOYEES_DISPLAYED ? employeesOfType.map((employee, i) => renderEmployee(employee, i)) : EMPLOYEES_MAX_PLACEHOLDER[type]}
         </p>
       </>
     )
+  );
+}
+
+function renderEmployee(employee, index) {
+  return (
+    <span key={`${employee.type}-${index}`} title={employee.name}>
+      {employee.image}
+    </span>
   );
 }
